3903088bd8899a18c1073ce28157ce4bf6e24858
isaac balam <manoel.balam@gmail.com>
2025-10-14 22:49:47 -0500

    GET & POST order endpoint working

--- dbaccess/orderDBA.py
+++ dbaccess/orderDBA.py
@@ -1,17 +1,27 @@
 from flask import jsonify
 from pymongo import MongoClient
-import pdb
+
 class orderMongo:
 
 	client = MongoClient('mongodb://127.0.0.1:27017/')
 	db = client['flask-mongo-api']
 	collectionOrder = db['order']
 
+
 	@staticmethod
 	def listOrders():
-		# pdb.set_trace()s
 		try:
-			order = list(orderMongo.collectionOrder.find())
-			return jsonify(order)
+			orders = list(orderMongo.collectionOrder.find())
+			for order in orders:
+				order['_id'] = str(order['_id'])
+			return jsonify(orders)
+		except Exception as e:
+			return 'Error: an error ecountred in the orderMongo:', 500
+
+	@staticmethod
+	def createOrder(body):
+		try:
+			order = orderMongo.collectionOrder.insert_one(body)
+			return	jsonify({"Message": "Order created successfully!", "id":str(order.inserted_id)}) , 201
 		except Exception as e:
 			return 'Error: an error ecountred in the orderMongo:', 500
