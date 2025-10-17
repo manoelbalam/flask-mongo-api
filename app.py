c032766f24078a485ab68bc190348ed1ed88c541
isaac balam <manoel.balam@gmail.com>
2025-10-17 01:35:20 -0500

    adding debig on persitence layer

--- app.py
+++ app.py
@@ -1,14 +1,78 @@
 from flask import Flask, jsonify, request
 from controllers.orderController import orderBP
 from flasgger import Swagger
+from logging.config import dictConfig
+from flask.logging import default_handler
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'console': {
+            'format': '%(message)s'
+        },
+        'default': {
+            'format': '[%(asctime)s] %(module)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+            'formatter': 'console',
+        },
+        'info': {
+            'class' : 'logging.FileHandler',
+            'formatter': 'default',
+            'filename' : 'logs/info.log',
+            'level'    : 'DEBUG'
+        },
+        'error': {
+            'class' : 'logging.FileHandler',
+            'formatter': 'default',
+            'filename' : 'logs/error.log',
+            'level'    : 'ERROR'
+        }
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console']
+    },
+    "loggers": {
+        "info_log": {
+            "level": "INFO",
+            "handlers": ["info"],
+            "propagate": False,
+        },
+        "error_log": {
+            "level": "ERROR",
+            "handlers": ["error"],
+            "propagate": False,
+        }
+    },
+})
 
 app = Flask(__name__)
+
+app.logger.removeHandler(default_handler)
 
 app.config['SWAGGER'] = {
     'title': 'flask-mongo-api',
     'description': 'API RESTful CRUD Orders',
     'version': 'v0.1',
     'uiversion': 2
+}
+
+app.config['FLASK_LOGGING_EXTRAS'] = {
+    'BLUEPRINT': {
+        'FORMAT_NAME': 'bp',
+        'APP_BLUEPRINT': '<app>',
+        'NO_REQUEST_BLUEPRINT': '<not a request>',
+    },
+    'RESOLVERS': {
+        'categoy': '<unset>',
+        'client': 'log_helper.get_client',
+    },
 }
 
 swagger = Swagger(app)
--- dbaccess/orderDBA.py
+++ dbaccess/orderDBA.py
@@ -1,8 +1,11 @@
+from bson import ObjectId
 from flask import jsonify
 from pymongo import MongoClient
+import logging
 import pdb
 
-from bson import ObjectId
+error_log = logging.getLogger("error_log")
+info_log = logging.getLogger("info_log")
 
 class orderMongo:
 
@@ -16,16 +19,20 @@ class orderMongo:
       orders = list(orderMongo.collectionOrder.find())
       for order in orders:
         order['_id'] = str(order['_id'])
+      info_log.info(f'orders retrieved:{len(orders)}')
       return jsonify(orders), 200
     except Exception as e:
+      error_log.error(str(e))
       return jsonify({"Error": str(e)}), 500
 
   @staticmethod
   def createOrder(body):
     try:
       order = orderMongo.collectionOrder.insert_one(body)
+      info_log.info(f'order _id:{order_id} created')
       return  jsonify({"Message": "Order created successfully!", "id":str(order.inserted_id)}) , 201
     except Exception as e:
+      error_log.error(str(e))
       return jsonify({"Error": str(e)}), 500
 
   @staticmethod
@@ -34,35 +41,39 @@ class orderMongo:
       order = orderMongo.collectionOrder.find_one({"_id": ObjectId(order_id)})
       if order:
         order['_id'] = str(order['_id'])
+        info_log.info(f'order _id:{order_id} retrieved')
         return jsonify(order), 200
+      info_log.info(f'order _id:{order_id} not found')
       return jsonify({"Error": "Order not found"}), 404
     except Exception as e:
+      error_log.error(str(e))
       return jsonify({"Error": str(e)}), 500
 
   @staticmethod
   def updateOrderByID(order_id, body):  
     try:
-      # data = request.json
       updated_data = {"$set": {"customer_name": body.get("customer_name"), "status": body.get("status")}}
-
       order = orderMongo.collectionOrder.update_one({"_id": ObjectId(order_id)}, updated_data)
-
       if order.matched_count:
           updated_order = orderMongo.collectionOrder.find_one({"_id": ObjectId(order_id)})
           updated_order['_id'] = str(updated_order['_id'])
+          info_log.info(f'order _id:{order_id} updated')
           return jsonify(updated_order), 200
+      info_log.info(f'order _id:{order_id} not found')
       return jsonify({"error": "Order not found"}), 404
     except Exception as e:
+      error_log.error(str(e))
       return jsonify({"Error": str(e)}), 500
 
   @staticmethod
   def deleteOrderByID(order_id):  
-    # pdb.set_trace()  
     try:
       order = orderMongo.collectionOrder.delete_one({"_id": ObjectId(order_id)})
-
       if order.deleted_count:
+        info_log.info(f'order _id:{order_id} deleted')
         return jsonify({"Message": "Order deleted successfully"})
+      info_log.info(f'order _id:{order_id} not found')
       return jsonify({"Error": "Order not found"}), 404
     except Exception as e:
+      error_log.error(str(e))
       return jsonify({"Error": str(e)}), 500
